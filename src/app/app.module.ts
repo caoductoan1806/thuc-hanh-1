12ac066908cd4811aad39bdd269668e60ce65c38
ndt-pro <toannguyen080199@gmail.com>
2020-09-04 10:43:53 +0700

    update bai 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Bai5Component } from './bai5/bai5.component';
 import { Bai1Component } from './bai1/bai1.component';
 import { Bai7Component } from './bai7/bai7.component';
 import { Bai6Component } from './bai6/bai6.component';
+import { Bai3Component } from './bai3/bai3.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Bai6Component } from './bai6/bai6.component';
     Bai5Component,
     Bai1Component,
     Bai7Component,
-    Bai6Component
+    Bai6Component,
+    Bai3Component
   ],
   imports: [
     BrowserModule,

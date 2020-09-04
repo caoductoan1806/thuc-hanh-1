86029cc37b8b9facc7e4e3ea1d13522602541626
ndt-pro <toannguyen080199@gmail.com>
2020-09-04 10:27:28 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Bai5Component } from './bai5/bai5.component';
+import { Bai1Component } from './bai1/bai1.component';
+import { Bai7Component } from './bai7/bai7.component';
+import { Bai6Component } from './bai6/bai6.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Bai5Component,
+    Bai1Component,
+    Bai7Component,
+    Bai6Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
